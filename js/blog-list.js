7a0dd7805a642e2777baea72f10d3f6cb8d25208
tbinh2906 <t.binhns@gmail.com>
2026-03-19 23:06:00 +0700

    Thêm các bài viết, hình ảnh và chỉnh sửa js

--- js/blog-list.js
+++ js/blog-list.js
@@ -1,18 +1,48 @@
 const BLOG_POSTS = [
     {
-        title: "Bài viết mẫu", // Tiêu đề bài viết
-        date: "9-8-2025", // Ngày tạo bài viết, luôn tuân theo định dạng dd-mm-yyyy để dễ dàng sắp xếp
+        title: "HANG MO SO: KIỆT TÁC ĐÁ VÔI VÀ HUYỀN THOẠI CĂN CỨ CÁCH MẠNG KIÊN LƯƠNG", // Tiêu đề bài viết
+        date: "19-3-2026", // Ngày tạo bài viết, luôn tuân theo định dạng dd-mm-yyyy để dễ dàng sắp xếp
         category: "Giới thiệu cảnh quan", // Thể loại bài viết
-        description: "abcxyzt", // Đoạn mô tả ngắn gọn bài viết
-        link: "blogs/bai-1.html", // Tên file của bài viết
-        image: "images/bai-1.jpg" // Ảnh bìa của bài viết (Đặt trùng tên file bài viết nhưng đuôi là file ảnh nhé)
+        description: "Hang Mo So là hệ thống hang động đá vôi kỳ vỹ tại huyện Kiên Lương, tỉnh Kiên Giang, nơi kết hợp độc đáo giữa tuyệt tác xâm thực của thiên nhiên triệu năm và di tích lịch sử căn cứ cách mạng hào hùng.", // Đoạn mô tả ngắn gọn bài viết
+        link: "blogs/Mosonew.html", // Tên file của bài viết
+        image: "images/1Moso.jpg" // Ảnh bìa của bài viết (Đặt trùng tên file bài viết nhưng đuôi là file ảnh nhé)
     },
+    // Thêm các bài khác vào đây...
+    
     {
-        title: "Phú Quốc", // Tiêu đề bài viết
-        date: "18-3-2026", // Ngày tạo bài viết, luôn tuân theo định dạng dd-mm-yyyy để dễ dàng sắp xếp
+        title: "HÒN PHỤ TỬ & CHÙA HANG: BIỂU TƯỢNG NGHĨA TÌNH VÀ TUYỆT TÁC TRONG LÒNG NÚI", // Tiêu đề bài viết
+        date: "19-3-2026", // Ngày tạo bài viết, luôn tuân theo định dạng dd-mm-yyyy để dễ dàng sắp xếp
         category: "Giới thiệu cảnh quan", // Thể loại bài viết
-        description: "Cảnh quan Phú Quốc rất đẹp", // Đoạn mô tả ngắn gọn bài viết
-        link: "blogs/bai-viet-phu-quoc.html", // Tên file của bài viết
-        image: "images/bai-viet-phu-quoc.jpg" // Ảnh bìa của bài viết (Đặt trùng tên file bài viết nhưng đuôi là file ảnh nhé)
+        description: "Hòn Phụ Tử và Chùa Hang là cụm danh thắng nổi tiếng tại Kiên Giang, nơi kết nối giữa không gian tâm linh huyền bí trong hang đá với biểu tượng thiêng liêng về tình phụ tử giữa biển trời mênh mông.",
+        link: "blogs/Honphutu.html", // Tên file của bài viết
+        image: "images/8Hòn Phụ Tử.jpg" // Ảnh bìa của bài viết (Đặt trùng tên file bài viết nhưng đuôi là file ảnh nhé)
+    },
+    // Thêm các bài khác vào đây...
+    
+    {
+        title: "NÚI ĐÁ DỰNG: \"ĐỘNG PHONG NHA\" GIỮA LÒNG PHÙ SA VÀ HUYỀN THOẠI BIÊN THÙY HÀ TIÊN", // Tiêu đề bài viết
+        date: "19-3-2026", // Ngày tạo bài viết, luôn tuân theo định dạng dd-mm-yyyy để dễ dàng sắp xếp
+        category: "Giới thiệu cảnh quan", // Thể loại bài viết
+        description: "Núi Đá Dựng là một trong những điểm đến không thể bỏ qua khi du lịch Hà Tiên, Kiên Giang. Được ví như “động Phong Nha trên vùng đất phù sa”, khu du lịch Núi Đá Dựng thu hút du khách bởi vẻ đẹp hoang sơ hùng vĩ và những câu chuyện lịch sử huyền thoại",
+        link: "blogs/Nuidadung.html", // Tên file của bài viết
+        image: "images/17Núi Đá Dựng.jpg" // Ảnh bìa của bài viết (Đặt trùng tên file bài viết nhưng đuôi là file ảnh nhé)
+    },
+    
+    {
+        title: "NÚI BÌNH SAN: \"BÌNH SAN ĐIỆP THÚY\" VÀ DẤU ẤN KHAI MỞ ĐẤT HÀ TIÊN", // Tiêu đề bài viết
+        date: "19-3-2026", // Ngày tạo bài viết, luôn tuân theo định dạng dd-mm-yyyy để dễ dàng sắp xếp
+        category: "Giới thiệu cảnh quan", // Thể loại bài viết
+        description: "Núi Bình San - lăng Mạc Cửu không chỉ là danh lam thắng cảnh mà còn chứa đựng nhiều câu chuyện ly kỳ được lưu truyền cho đến ngày nay.",
+        link: "blogs/Nuibinhsan.html", // Tên file của bài viết
+        image: "images/21Bình San.jpg", // Ảnh bìa của bài viết (Đặt trùng tên file bài viết nhưng đuôi là file ảnh nhé)
+    },
+    
+    {
+        title: "NHỮNG \"HÒN NGỌC\" VIỄN CHINH GIỮA TRÙNG KHƠI KIÊN GIANG", // Tiêu đề bài viết
+        date: "19-3-2026", // Ngày tạo bài viết, luôn tuân theo định dạng dd-mm-yyyy để dễ dàng sắp xếp
+        category: "Giới thiệu cảnh quan", // Thể loại bài viết
+        description: "Hệ thống các hòn đảo tại Kiên Giang là thiên đường nghỉ dưỡng đa dạng, từ vẻ hoang sơ kỳ bí của Đảo Hải Tặc đến sự hiện đại, sôi động của Hòn Thơm.",
+        link: "blogs/Dao.html", // Tên file của bài viết
+        image: "images/Đ9.jpg" // Ảnh bìa của bài viết (Đặt trùng tên file bài viết nhưng đuôi là file ảnh nhé)
     },
 ];
